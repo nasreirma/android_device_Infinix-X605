e19f893a77c65a84b7f10fe16db1e6039d559e04
Nasrellah Irhmar <nasreirma@gmail.com>
2019-06-26 11:08:40 +0100

    init

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/omni_Infinix-X605.mk
---
+++ omni_Infinix-X605.mk
@@ -0,0 +1,21 @@
+# Release name
+PRODUCT_RELEASE_NAME := Infinix-X605
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Infinix
+PRODUCT_DEVICE := H632
+PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
+PRODUCT_MODEL := Infinix-X605
+PRODUCT_NAME := omni_Infinix-X605
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0
